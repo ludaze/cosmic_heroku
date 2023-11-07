089bd2e65585a53a1e9866516c3e8f156706f8c3
Ludaze <luhanad@ymail.com>
2023-11-07 15:19:22 +0300

    order and purchase forms created

--- cosmic/forms.py
+++ cosmic/forms.py
@@ -34,3 +34,21 @@ class SupplierForm(forms.ModelForm):
    
         model = cosmic_supplier_profile
         fields = ['supplier_name','supplier_address','email','phone_number','contact_person','comments']
+
+class CosmicOrderForm(forms.ModelForm):
+    
+     order_no = forms.CharField(widget=forms.TextInput(attrs={'class': 'order_no form-control'}))
+
+    class Meta:
+   
+        model = cosmic_order
+        fields = ['order_no','date','payment_type','measurement_type','approved_by','PR_before_vat','transportation','shipment_type']
+
+class CosmicPurchaseForm(forms.ModelForm):
+    
+    purchase_no = forms.CharField(widget=forms.TextInput(attrs={'class': 'purchase_no form-control'}))
+
+    class Meta:
+   
+        model = cosmic_purchase
+        fields = ['purchase_no','date','payment_type','measurement_type','before_vat','transportation','shipment_type']
