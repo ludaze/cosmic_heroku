21390db4bfa8d0be0c23a91fce134b6485209ea4
Ludaze <luhanad@ymail.com>
2023-11-08 13:15:57 +0300

    create customer view

--- cosmic/views.py
+++ cosmic/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create_customer(request):
+    
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.errors:
+            print(form.errors)
+        if form.is_valid():
+            try:
+                form.save()
+            except Exception as e:
+                print(f"Error: {e}")
+            return redirect('create_customer')
+    
+    else:
+        
+        form = CustomerForm()
+    return render(request, 'create_customer.html', {'form': form })
