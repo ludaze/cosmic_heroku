fa3f08b02b1a55bc27be6bdd68938d955f14a0b0
Ludaze <luhanad@ymail.com>
2023-11-15 15:53:38 +0300

    order item form integration

--- cosmic/forms.py
+++ cosmic/forms.py
@@ -44,6 +44,29 @@ class CosmicOrderForm(forms.ModelForm):
         model = cosmic_order
         fields = ['customer_name','total_price','order_no','date','payment_type','measurement_type','approved_by','PR_before_vat','transportation','shipment_type']
 
+class OrderItemForm(forms.ModelForm):
+   
+    before_vat = forms.DecimalField(
+        label='Total Price',
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'before_vat form-control', 'readonly': 'readonly'})
+    )
+    measurement = forms.CharField(widget=forms.TextInput(attrs={'class': 'measurement form-control'}), required=False)
+    quantity = forms.FloatField(widget=forms.TextInput(attrs={'class': 'quantity form-control' }))
+    price = forms.DecimalField(widget=forms.TextInput(attrs={'class': 'price form-control'}))
+
+    item_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter name'}),
+    )
+    hs_code = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'HS-CODE'}),
+    )
+    
+    class Meta:
+   
+        model = order_item
+        fields = ['total_price', 'item_name','hs_code','price','quantity','before_vat','measurement']
+
 class CosmicPurchaseForm(forms.ModelForm):
     
     purchase_no = forms.CharField(widget=forms.TextInput(attrs={'class': 'purchase_no form-control'}))
