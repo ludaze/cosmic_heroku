faf73627f3ddde2a6737ebed427792ad03fe1927
Ludaze <luhanad@ymail.com>
2023-11-28 14:45:29 +0300

    changed fields of places on shipping

--- cosmic/models.py
+++ cosmic/models.py
@@ -38,7 +38,10 @@ class cosmic_order(models.Model):
     total_quantity = models.FloatField(blank=True, null=True)
     remaining =  models.FloatField(blank=True, null=True)
     supplier_name = models.ForeignKey('supplier_profile', related_name='orders_related_to_supplier',on_delete=models.CASCADE, blank=False, null=True,db_column='supplier_name')
-    
+    port_of_loading = models.TextField(blank=False, null=True)
+    port_of_discharge = models.TextField(blank=True, null=True)
+    final_destination = models.TextField(blank=True, null=True)
+    country_of_origin = models.TextField(blank=False, null=True)
     
 class cosmic_purchase(models.Model):
     supplier_name = models.ForeignKey('supplier_profile', on_delete=models.CASCADE, db_column='supplier_name',blank=True, null=True)
@@ -56,14 +59,17 @@ class cosmic_purchase(models.Model):
 class shipping_info(models.Model):
     unique_no = models.AutoField(primary_key=True)
     invoice_date = models.DateField(blank=True, null=True)
+    container_no = models.IntegerField(blank=True, null=True)
+    truck_waybill_no = models.TextField(blank=True, null=True)
+    customer_no = models.TextField(blank=True, null=True)
+    freight_amount = models.FloatField(blank=True, null=True)
+    vessel = models.TextField(blank=True, null=True)
+    total_net_weight = models.FloatField(blank=True, null=True)
+    total_gross_weight = models.FloatField(blank=True, null=True)
     port_of_loading = models.TextField(blank=False, null=True)
     port_of_discharge = models.TextField(blank=True, null=True)
     final_destination = models.TextField(blank=True, null=True)
-    container_no = models.IntegerField(blank=True, null=True)
-    truck_waybill_no = models.TextField(blank=True, null=True)
     country_of_origin = models.TextField(blank=False, null=True)
-    customer_no = models.TextField(blank=True, null=True)
-    freight_amount = models.FloatField(blank=True, null=True)
     
 class order_item(models.Model):
     order_no = models.ForeignKey('cosmic_order', on_delete=models.CASCADE, db_column='order_no',blank=True, null=True)
