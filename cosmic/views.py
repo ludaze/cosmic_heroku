e5b3fa1b773866b761a06feedc371f2335500bd0
Ludaze <luhanad@ymail.com>
2023-12-06 15:04:38 +0300

    num to words conversion on prices

--- cosmic/views.py
+++ cosmic/views.py
@@ -203,24 +203,52 @@ def create_order_items(request):
 def display_single_order(request):
     if request.method == 'GET':
         pr_no = request.GET['order_no']
-        orders = cosmic_order.objects.get(order_no=pr_no)
-        pr_items = order_item.objects.all()
-        pr_items = pr_items.filter(order_no=pr_no)
+        print(pr_no,"nada")
             
-        
+        try:
+            orders = cosmic_order.objects.get(order_no=pr_no)
+            pr_items = order_item.objects.all()
+            pr_items = pr_items.filter(order_no=pr_no)
+            print(pr_items)
+        except cosmic_order.DoesNotExist:
+            # If it's not found in purchase_orders, try searching in import_PR
+            try:
+                orders = cosmic_purchase.objects.get(purchase_no=pr_no)
+                print(orders,"not in")
+                pr_items = purchase_item.objects.all()
+                pr_items = pr_items.filter(purchase_no=pr_no)
+                print(pr_items)
+            except cosmic_purchase.DoesNotExist:
+                order = None
+            order = None 
+        try:
+            
+            invoices = shipping_info.objects.all()
+            invoices = invoices.filter(order_no = pr_no)
+        except shipping_info.DoesNotExist:
+            try:
+                print("trial")
+                invoices = shipping_info.objects.all()
+                invoices = invoices.filter(order_no = pr_no)
+            except shipping_info.DoesNotExist:
+                invoices = None
+            invoices = None
+        print(orders)
+        print("no")
         if pr_items.exists():
             print(pr_items,"yes")
             context = {
                         'pr_items': pr_items,
                         'my_order': orders,
+                        'the_invoices':invoices,
                     }
             return render(request, 'display_single_order.html', context)
         context = {
                         
                         'my_order': orders,
+                        'the_invoices':invoices
                     }
     return render(request, 'display_single_order.html', context)
-
 def create_shipping(request):
     if request.method == 'POST':
         ship_form = ShippingForm(request.POST)
@@ -447,9 +475,7 @@ def print_order(request):
             number += float(orders.freight_price)
         #print(shipping.freight_amount,"fr")
         dicts = {1:"TEN",2:"TWENTY",3:"THIRTY",4:"FORTY",5:"FIFTY",6:"SIXTY",7:"SEVENTY",8:"EIGHTY",9:"NINTY"}
-        # if shipping:
-        #     if orders.freight_price:
-        #         number += (orders.freight_price)
+        
         print(number)
         whole_part, decimal_part = str(number).split('.')
         number_in_words = num2words(whole_part)
