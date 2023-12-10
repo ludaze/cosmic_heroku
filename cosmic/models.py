d439c93f5f9805c8621e1208d5a3bdbcd240d702
Ludaze <luhanad@ymail.com>
2023-12-11 02:40:54 +0300

    primary key for shipping changed

--- cosmic/models.py
+++ cosmic/models.py
@@ -58,7 +58,7 @@ class cosmic_purchase(models.Model):
     status = models.TextField(blank=True, null=True, default="Pending")
 
 class shipping_info(models.Model):
-    unique_no = models.AutoField(primary_key=True)
+    unique_no = models.TextField(blank=False, null=True)
     invoice_date = models.DateField(blank=True, null=True)
     order_no = models.ForeignKey('cosmic_order', on_delete=models.CASCADE, db_column='order_no',blank=True, null=True)
     invoice_num = models.TextField(primary_key=True)
