31d2fa9c4bd6b5632f7f736c4d0d7f13fd884c95
tibarekb <berassut@gmail.com>
2024-04-11 08:50:09 +0300

    creation date added

--- cosmic/models.py
+++ cosmic/models.py
@@ -6,16 +6,17 @@ class customer_profile(models.Model):
     customer_name = models.TextField(blank=True,primary_key=True)
     customer_address = models.TextField(blank=True)
     contact_person = models.TextField(blank=True)
-    phone_number = models.CharField(blank=True, null=True)  # This field type is a guess.
+    phone_number = models.CharField(blank=True, null=True, max_length=12 )  # This field type is a guess.
     email = models.TextField(blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
 
 class supplier_profile(models.Model):
     #supplier_id = models.AutoField(primary_key=True)
     supplier_name = models.TextField(primary_key=True)
     supplier_address = models.TextField(blank=True)
     contact_person = models.TextField(blank=True)
-    phone_number = models.CharField(blank=True, null=True)  # This field type is a guess.
+    phone_number = models.CharField(blank=True, null=True, max_length=12)  # This field type is a guess.
     email = models.TextField(blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
 
