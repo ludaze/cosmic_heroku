2a3779cf2743951488ccc72982bb53f2c06bb2c3
tibarekb <berassut@gmail.com>
2024-04-25 16:28:15 +0300

    shipping error configured

--- cosmic/admin.py
+++ cosmic/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import cosmic_order,cosmic_purchase,order_item,customer_profile,supplier_profile
+from .models import cosmic_order,cosmic_purchase,order_item,customer_profile,supplier_profile,shipping_info, order_item, purchase_item
+from .models import invoice_item, item_codes
 # Register your models here.
 admin.site.register(cosmic_order)
 admin.site.register(cosmic_purchase)
 admin.site.register(order_item)
 admin.site.register(customer_profile)
 admin.site.register(supplier_profile)
+admin.site.register(shipping_info)
+admin.site.register(purchase_item)
+admin.site.register(invoice_item)
+admin.site.register(item_codes)
+
